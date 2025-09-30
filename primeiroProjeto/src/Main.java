9379232d42ef0b7ea00c3eb8fcd935b5276e53fc
Lucas Beninca <lucasbeninca@proton.me>
2025-09-30 07:49:04 -0300

    Variaveis Tipos primitivos Operadores Lógicos e Aritéticos

--- primeiroProjeto/src/Main.java
+++ primeiroProjeto/src/Main.java
@@ -1,5 +1,22 @@
 public  class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: John Wick 4");
+
+        int anoDeLancamento = 2023;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento + "\n");
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 9.0) /3;
+        System.out.println("Media: " + media);
+        String sinopse;
+        sinopse = """
+                    Filme Jhon Wick
+                    Filme de ação
+                    Muito Bom!
+                    Ano de Lançamento:
+                   """ + anoDeLancamento;
+        System.out.println("Sinopse: " + sinopse);
     }
 }
---
+++ primeiroProjeto/src/Soma.java
@@ -0,0 +1,11 @@
+public class Soma {
+    public static void main(String[] args) {
+        var number1 = new Integer(10);
+        var number2 = new Integer(5);
+
+        System.out.println(number1 - number2);
+
+
+
+    }
+}
