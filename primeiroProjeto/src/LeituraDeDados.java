deb31a931f583bfff6d97f385fb38b1506c93eae
Lucas Beninca <lucasbeninca@proton.me>
2025-10-01 14:28:18 -0300

    inputando dados

--- primeiroProjeto/src/LeituraDeDados.java
+++ primeiroProjeto/src/LeituraDeDados.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 public class LeituraDeDados {
     static void main(String[] args) {
         Scanner inputDeDados = new Scanner(System.in);
+
         System.out.println("Digite seu filme favorito: ");
         String nomeFilm = inputDeDados.nextLine();
         System.out.println(nomeFilm);
+        System.out.println("Qual ano de lancamento? ");
+        int anoDeLancamento = inputDeDados.nextInt();
+        System.out.println("Qual nota você da para o filme? ");
+        double notaParaOFilme = inputDeDados.nextDouble();
+
+        System.out.println(nomeFilm);
+        System.out.println(anoDeLancamento);
+        System.out.println(notaParaOFilme);
+
+
     }
 }
